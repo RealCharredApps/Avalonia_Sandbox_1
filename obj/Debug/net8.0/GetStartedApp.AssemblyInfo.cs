bc386f7fe9269d32911eeef6440cce15e39146d9
Justin Sjaaheim <justinsjaaheim@icloud.com>
2025-04-17 18:24:01 -0500

    Step 2 — Added: App Model Controller Data Folder Structure

--- obj/Debug/net8.0/GetStartedApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/GetStartedApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d837f29589070ffb133d491e07e8eadd74f00cb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GetStartedApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
